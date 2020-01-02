7d876d34c15c433a2d53468cf413b586d53eb741
rlaxodnd95@naver.com <rlaxodnd95@naver.com>
2020-01-02 21:30:31 +0900

    백준 2798 블랙잭

--- AlgorithmStudy/src/Main.java
+++ AlgorithmStudy/src/Main.java
@@ -1,24 +1,54 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String dayNum = bf.readLine();
-		String carNum = bf.readLine();
-		String[] carNumArray = carNum.split(" ");
-		int result = 0;
-		for (int i=0; i<carNumArray.length; i++){
-			if(Integer.parseInt(dayNum) == Integer.parseInt(carNumArray[i])) {
-				result++;
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st1.nextToken());
+        int M = Integer.parseInt(st1.nextToken());
 
-		System.out.println(result);
+        String numberString[] = bf.readLine().split(" ");
+        int number[] = Arrays.stream(numberString).mapToInt(Integer::parseInt).toArray();
+        int min = 0;
 
+        boolean flag = true;
+        for (int i = N-1; i >= 0; i--) {
+            for (int j = N-2; j >= 0 ; j--) {
+                if (i == j) {
+                    continue;
+                }
+                for (int k = N-3; k >= 0 ; k--) {
+                    if (i == k || i ==j || j == k) {
+                        continue;
+                    }
+                    if (number[i] + number[j] + number[k] <= M) {
+                        if (min < number[i] + number[j] + number[k]) {
+                            min = number[i] + number[j] + number[k];
+//                            System.out.println("최소 : " + min + "," + + number[i] + number[j] + number[k]);
+                        }
+                        if (number[i] + number[j] + number[k] == M) {
+//                            System.out.println("똑같다! : " + number[i] + number[j] + number[k] );
+                            min = number[i] + number[j] + number[k];
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if (flag == false) {
+                    break;
+                }
+            }
+            if (flag == false) {
+                break;
+            }
+        }
+        System.out.println(min);
+    }
 
-	}
 
 }
