42a05f43a696bd8b88862cca85d83c94a476d41c
김태웅 <rlaxodnd95@gmail.com>
2021-08-24 14:19:46 +0900

    백준 1330번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_1330.java
@@ -0,0 +1,30 @@
+package com.taewoong.baekjoon.bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = bufferedReader.readLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(s);
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+
+        if (a > b) {
+            bufferedWriter.write(">");
+        } else if (a < b) {
+            bufferedWriter.write("<");
+        } else {
+            bufferedWriter.write("==");
+        }
+        bufferedWriter.flush();
+
+        bufferedWriter.close();
+        bufferedReader.close();
+    }
+}
