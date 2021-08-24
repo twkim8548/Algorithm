811a7cdf2d2d9c3e8e8d4a2b340f4df50af479ef
김태웅 <ted@softsquared.com>
2021-08-24 14:10:52 +0900

    백준 10171번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_10171.java
@@ -0,0 +1,19 @@
+package com.taewoong.baekjoon.bronze;
+
+import java.io.*;
+
+public class _10171 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bufferedWriter.write("\\    /\\\n");
+        bufferedWriter.write(" )  ( ')\n");
+        bufferedWriter.write("(  /  )\n");
+        bufferedWriter.write(" \\(__)|\n");
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+    }
+}
