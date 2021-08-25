70a40e44ff4dde94c8dc679bf8ce6e109108021c
김태웅 <rlaxodnd95@gmail.com>
2021-08-26 00:25:27 +0900

    백준 10952번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_10952.kt
@@ -0,0 +1,25 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    while (true) {
+        val token = StringTokenizer(br.readLine())
+        val a = token.nextToken().toInt()
+        val b = token.nextToken().toInt()
+        if (a == 0 && b == 0) break;
+        val sum = (a + b).toString()
+
+        bw.write(sum + '\n')
+    }
+
+    bw.flush()
+    bw.close()
+}
