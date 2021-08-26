540c09bd4abdbb1199092bb0ff39b2157a0de042
김태웅 <rlaxodnd95@gmail.com>
2021-08-26 19:40:54 +0900

    백준 10818번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_10818.kt
@@ -0,0 +1,27 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    val array = mutableListOf<Int>()
+    val token = StringTokenizer(br.readLine())
+    repeat(n) {
+        array.add(token.nextToken().toInt())
+    }
+
+    array.sort()
+
+    bw.write("${array[0]} ${array[array.size-1]}")
+    bw.flush()
+    bw.close()
+    br.close()
+}
+
