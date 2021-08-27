e5d682d007a3d3e1e92cb941dc626748e463803f
김태웅 <rlaxodnd95@gmail.com>
2021-08-27 18:58:19 +0900

    백준 11654번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_11654.kt
@@ -0,0 +1,18 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = br.readLine().single()
+
+    bw.write("${a.toInt()}")
+    bw.flush()
+    bw.close()
+    br.close()
+}
