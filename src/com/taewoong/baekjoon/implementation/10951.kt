52b0dcfc0070341469b2f44c52b832ee9a34ff36
김태웅 <rlaxodnd95@gmail.com>
2021-08-27 18:40:33 +0900

    백준 10951번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/10951.kt
@@ -0,0 +1,22 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    var s: String?
+    while ( br.readLine().also { s = it } != null) {
+        val token = StringTokenizer(s)
+        val a = token.nextToken().toInt()
+        val b = token.nextToken().toInt()
+
+        bw.write("${a + b}\n")
+        bw.flush()
+    }
+}
