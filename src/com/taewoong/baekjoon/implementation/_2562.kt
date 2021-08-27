e79605e826a6a697d24c93161e4cdb45b474d50c
김태웅 <rlaxodnd95@gmail.com>
2021-08-27 19:12:53 +0900

    백준 2562번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_2562.kt
@@ -0,0 +1,26 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val array = ArrayList<Int>()
+
+    repeat(9) {
+        array.add(br.readLine().toInt())
+    }
+
+    val a = array.max()
+    val b = array.indexOf(array.max()) + 1
+    bw.write("${a}\n${b}")
+
+    bw.flush()
+    bw.close()
+    br.close()
+
+}
