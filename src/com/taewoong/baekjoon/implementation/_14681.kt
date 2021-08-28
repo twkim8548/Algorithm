ca8c9bd04e0f05f90af9122acc49722dbe0aea36
김태웅 <rlaxodnd95@gmail.com>
2021-08-28 20:20:39 +0900

    백준 14681번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_14681.kt
@@ -0,0 +1,28 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = br.readLine().toInt()
+    val b = br.readLine().toInt()
+
+    if (a > 0 && b > 0) {
+        bw.write("1")
+    } else if (a < 0 && b > 0) {
+        bw.write("2")
+    } else if (a < 0 && b < 0) {
+        bw.write("3")
+    } else if (a > 0 && b < 0) {
+        bw.write("4")
+    }
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
