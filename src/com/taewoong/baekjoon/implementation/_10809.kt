22ed02a5a34cf6801ddca2fabbafbda45cc6670b
김태웅 <rlaxodnd95@gmail.com>
2021-08-30 12:44:29 +0900

    백준 10809번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/implementation/_10809.kt
@@ -0,0 +1,39 @@
+package com.taewoong.baekjoon.implementation
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val s = br.readLine()
+
+    val answer = Array<Int>(26) { -1 }
+
+    s.forEachIndexed() { i, c ->
+        var alphabet = 'a'
+        while (alphabet <= 'z') {
+            if (answer[c.toInt() - 97] >= 0) {
+                break
+            }
+            if (c == alphabet) {
+                answer[c.toInt() - 97] = i
+                break
+            }
+            ++alphabet
+        }
+    }
+
+
+    answer.forEach {
+        bw.write("$it ")
+    }
+
+    bw.flush()
+    bw.close()
+
+    br.close()
+}
