5e5be39214f074fe7824c755a4fb11b72f68d939
김태웅 <rlaxodnd95@gmail.com>
2021-09-11 20:42:10 +0900

    백준 5337번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_5337.kt
@@ -0,0 +1,13 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+
+    bw.write(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.\n")
+    bw.flush()
+    bw.close()
+}
