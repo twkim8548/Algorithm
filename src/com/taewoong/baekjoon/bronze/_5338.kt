a41fbcb85daced4b522b67c4f1617ceff07d535b
김태웅 <rlaxodnd95@gmail.com>
2021-09-12 23:44:14 +0900

    백준 5338번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_5338.kt
@@ -0,0 +1,17 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+
+    bw.write("       _.-;;-._\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-;;-._|\n" +
+            "'-..-'|   ||   |\n" +
+            "'-..-'|_.-''-._|")
+    bw.flush()
+    bw.close()
+}
