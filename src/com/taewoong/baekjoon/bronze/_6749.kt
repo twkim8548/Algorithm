4298e8222d7945dc149eef5ad44b90cbb4a36fa4
김태웅 <rlaxodnd95@gmail.com>
2021-09-25 21:41:32 +0900

    백준 6749번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_6749.kt
@@ -0,0 +1,20 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = br.readLine().toInt()
+    val b = br.readLine().toInt()
+
+    bw.write("${b + (b - a)}")
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
