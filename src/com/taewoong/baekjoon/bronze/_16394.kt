b1baf629b75c06b9c8f1ac47faade9ae550c9916
김태웅 <rlaxodnd95@gmail.com>
2021-10-02 10:38:14 +0900

    백준 16394번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_16394.kt
@@ -0,0 +1,17 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+
+    bw.write("${br.readLine().toInt() - 1946}")
+    bw.flush()
+    bw.close()
+    br.close()
+}
