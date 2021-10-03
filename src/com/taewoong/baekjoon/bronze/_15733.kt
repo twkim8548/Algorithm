1949eb07cac26d19741d4ace2fc92082f2898886
김태웅 <rlaxodnd95@gmail.com>
2021-10-03 23:12:35 +0900

    백준 15733번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_15733.kt
@@ -0,0 +1,12 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("I'm Sexy")
+    bw.flush()
+    bw.close()
+}
