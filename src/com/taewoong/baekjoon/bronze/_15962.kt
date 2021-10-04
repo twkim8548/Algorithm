e5c9a32911c12c36ca250ad42116c8dca93dd8fd
김태웅 <rlaxodnd95@gmail.com>
2021-10-04 11:03:02 +0900

    백준 15962번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_15962.kt
@@ -0,0 +1,11 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    bw.write("파이팅!!")
+    bw.flush()
+    bw.close()
+}
