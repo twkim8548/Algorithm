da05f841acc9d8d415778e6790e0fd69ea36581e
김태웅 <rlaxodnd95@gmail.com>
2021-10-15 10:46:41 +0900

    백준 10989번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/silver/_10989.kt
@@ -0,0 +1,27 @@
+package com.taewoong.baekjoon.silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val array = IntArray(br.readLine().toInt())
+
+    repeat(array.size) {
+        array[it] = br.readLine().toInt()
+    }
+
+    array.sort()
+
+    array.forEach {
+        bw.write("$it\n")
+    }
+    bw.flush()
+    bw.close()
+
+    br.close()
+}
