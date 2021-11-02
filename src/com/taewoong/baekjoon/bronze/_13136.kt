95c362f78ecb24b7faa11bb5cb1769b1279eddb5
김태웅 <rlaxodnd95@gmail.com>
2021-11-02 16:06:55 +0900

    백준 13136번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_13136.kt
@@ -0,0 +1,29 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.ceil
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+
+    val r = token.nextToken().toDouble()
+    val c = token.nextToken().toDouble()
+    val n = token.nextToken().toDouble()
+
+    val answer= ceil((r/n)).toInt() * ceil((c/n)).toInt()
+
+    bw.write("$answer")
+    bw.flush()
+    bw.close()
+
+    br.close()
+}
+
+
