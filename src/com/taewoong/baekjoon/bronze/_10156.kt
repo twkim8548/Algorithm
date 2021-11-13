0fd90ccb01d9baa9332ec067dea8ad7472981698
김태웅 <rlaxodnd95@gmail.com>
2021-11-13 17:55:09 +0900

    백준 10156번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_10156.kt
@@ -0,0 +1,27 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+
+    bw.write("${getNecessaryMoney(token.nextToken().toInt(), token.nextToken().toInt(), token.nextToken().toInt())}")
+    bw.flush()
+    bw.close()
+
+    br.close()
+
+}
+
+fun getNecessaryMoney(price: Int, number: Int, currentMoney: Int): Int {
+
+    val totalPrice = price * number
+    return if (totalPrice - currentMoney <= 0) 0 else totalPrice - currentMoney
+}
