cece90090ea19f8131691987bfe21fb9da460744
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2021-11-21 15:28:14 +0900

    Create _20254.kt

---
+++ src/com/taewoong/baekjoon/bronze/_20254.kt
@@ -0,0 +1,20 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    
+    val token = StringTokenizer(br.readLine())
+
+    bw.write("${56*token.nextToken().toInt() + 24*token.nextToken().toInt() + 14*token.nextToken().toInt() + 6*token.nextToken().toInt()}")
+    bw.flush()
+    bw.close()
+    
+	br.close()
+}
