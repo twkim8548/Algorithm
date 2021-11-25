d3b3b8df6e62c68a17f3afe6914d594cbf5c660f
김태웅 <rlaxodnd95@gmail.com>
2021-11-25 22:16:52 +0900

    백준 17496번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_17496.kt
@@ -0,0 +1,19 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+    bw.write("${((token.nextToken().toInt() - 1) / token.nextToken().toInt()) * token.nextToken().toInt() * token.nextToken().toInt()}")
+    bw.flush()
+    bw.close()
+
+    br.close()
+}
