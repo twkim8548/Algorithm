f23e74884e42e79c43225ed28a96d67778b13a23
김태웅 <rlaxodnd95@gmail.com>
2021-11-28 08:00:28 +0900

    백준 18096번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_18096.kt
@@ -0,0 +1,14 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("1")
+    bw.flush()
+    bw.close()
+}
