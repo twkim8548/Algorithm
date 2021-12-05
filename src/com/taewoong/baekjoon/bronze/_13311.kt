7583cf369ea0253cb38eb6031a434d367d2122fa
김태웅 <rlaxodnd95@gmail.com>
2021-12-05 12:05:32 +0900

    백준 13311번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_13311.kt
@@ -0,0 +1,13 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("-1")
+    bw.flush()
+    bw.close()
+
+}
