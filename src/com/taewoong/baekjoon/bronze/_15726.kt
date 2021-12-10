8e219258b0940ef7717899c6fcb8e3b9004ccd74
김태웅 <rlaxodnd95@gmail.com>
2021-12-10 22:24:04 +0900

    백준 15726번 문제 풀이 추가

--- src/com/taewoong/baekjoon/bronze/_15726.kt
+++ src/com/taewoong/baekjoon/bronze/_15726.kt
@@ -4,29 +4,22 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.math.max
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    val n = br.readLine().toInt()
-    var a = n / 10
-    var b = n % 100
+    val token = StringTokenizer(br.readLine())
+    val a = token.nextToken().toDouble()
+    val b = token.nextToken().toDouble()
+    val c = token.nextToken().toDouble()
 
-    if (a > 10) {
-        a /= 10
-    }
-
-    if (b > 10) {
-        b %= 10
-    }
-
-    bw.write("${a + b}")
-
-
-
+    bw.write("${max((a * b / c), (a / b * c)).toInt()}")
     bw.flush()
     bw.close()
 
     br.close()
+
 }
