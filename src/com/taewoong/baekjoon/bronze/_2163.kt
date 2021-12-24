e86d63bf65c4c588df64d022fc2c0f80d2f09a19
김태웅 <rlaxodnd95@gmail.com>
2021-12-24 11:03:36 +0900

    백준 2163번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_2163.kt
@@ -0,0 +1,21 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+    val n = token.nextToken().toInt()
+    val m =token.nextToken().toInt()
+
+    bw.write("${(m-1) + m *(n-1)}")
+
+    bw.flush()
+    bw.close()
+}
