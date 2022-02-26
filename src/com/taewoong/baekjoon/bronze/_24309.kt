29f06adb7eabf515778a71863f8f053812f50d0b
twkim8548 <rlaxodnd95@gmail.com>
2022-02-26 09:29:22 +0900

    백준 24309번 문제 풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_24309.kt
@@ -0,0 +1,19 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = br.readLine().toBigInteger()
+    val b = br.readLine().toBigInteger()
+    val c = br.readLine().toBigInteger()
+    bw.write("${(b-c)/a}")
+    bw.flush()
+    bw.close()
+    br.close()
+}
