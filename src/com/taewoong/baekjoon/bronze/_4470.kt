30181b373fbb6c607e3dc094e9dcd4c61c821c21
twkim8548 <rlaxodnd95@gmail.com>
2022-04-12 17:51:41 +0900

    백준 4470번 문제풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_4470.kt
@@ -0,0 +1,18 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(br.readLine().toInt()) {
+        bw.write("${it + 1}. ${br.readLine()}\n")
+    }
+    bw.flush()
+    bw.close()
+    br.close()
+}
