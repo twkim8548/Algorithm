3838ab3594e83c661d295a7fb2c3f7b8e705ecbb
twkim8548 <rlaxodnd95@gmail.com>
2022-05-09 19:36:46 +0900

    백준 10798번 문제풀이 추가

---
+++ src/com/taewoong/baekjoon/bronze/_10798.kt
@@ -0,0 +1,23 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val array = arrayOf(br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine())
+    for (i in 0 until array.maxOf { it.length }) {
+        repeat(5) { index ->
+            if (array[index].length > i)
+                bw.write("${array[index][i]}")
+        }
+    }
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
