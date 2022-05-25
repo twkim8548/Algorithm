86a8a1d4d173e3f73f3536d0e9dfb3581449b483
twkim8548 <rlaxodnd95@gmail.com>
2022-05-25 19:23:10 +0900

    백준 4335번 문제풀이 중간 저장

---
+++ src/com/taewoong/baekjoon/silver/_4335.kt
@@ -0,0 +1,48 @@
+package com.taewoong.baekjoon.silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val map = mutableMapOf<Int, String>()
+    while (true) {
+        val number = br.readLine().toInt()
+        if (number == 0) {
+            break
+        }
+        val answer = br.readLine()
+        if (answer == "right on") {
+            var isLie = false
+            map.forEach {
+                if (it.key > number) {
+                    if (it.value != "too high") {
+                        isLie = true
+                    }
+                } else if (it.key < number) {
+                    if (it.value != "too low") {
+                        isLie = false
+                    }
+                }
+            }
+            if (isLie) {
+                bw.write("Stan is dishonest\n")
+            } else {
+                bw.write("Stan may be honest\n")
+            }
+            map.clear()
+        } else {
+            map[number] = answer
+        }
+    }
+
+    bw.flush()
+    bw.close()
+
+    br.close()
+
+}
