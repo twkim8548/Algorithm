bc2180fdc14b93b9ea52a16681fe2e24756a1513
twkim8548 <rlaxodnd95@gmail.com>
2022-06-07 21:47:24 +0900

    백준 13909번 문제풀이 중간 저장

---
+++ src/com/taewoong/baekjoon/silver/_13909.kt
@@ -0,0 +1,20 @@
+package com.taewoong.baekjoon.silver
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.floor
+import kotlin.math.sqrt
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+
+    bw.write("${floor(sqrt(n.toDouble())).toInt()}")
+    bw.flush()
+    bw.close()
+    br.close()
+}
