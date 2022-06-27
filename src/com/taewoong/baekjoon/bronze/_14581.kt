d898e176dab63210e20707cf938d6863da45e950
twkim8548 <rlaxodnd95@gmail.com>
2022-06-27 20:22:22 +0900

    백준 14581번 문제풀이 저장

---
+++ src/com/taewoong/baekjoon/bronze/_14581.kt
@@ -0,0 +1,25 @@
+package com.taewoong.baekjoon.bronze
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val name = br.readLine()
+
+        repeat(3) {
+            bw.write(":fan:")
+        }
+        bw.write("\n:fan::$name::fan:\n")
+        repeat(3) {
+            bw.write(":fan:")
+        }
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
