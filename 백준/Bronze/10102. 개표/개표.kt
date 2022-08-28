f3bb805d3f49307e2175c960d444cb2b489298d6
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-08-28 14:59:34 +0900

    [Bronze II] Title: 개표, Time: 92 ms, Memory: 12192 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.kt
@@ -0,0 +1,27 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val v = br.readLine().toInt()
+    val vote = br.readLine()
+    var aCnt = 0
+    var bCnt = 0
+    vote.forEach {
+        if (it =='A') aCnt++
+        else bCnt++
+    }
+
+    if (aCnt > bCnt) bw.write("A")
+    else if (bCnt > aCnt) bw.write("B")
+    else bw.write("Tie")
+
+    bw.flush()
+    bw.close()
+
+    br.close()
+}
