6d712350c8f859a06e3daa20d03cdeaa11a1fd81
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-10-10 15:25:26 +0900

    [Bronze II] Title: 원피스, Time: 188 ms, Memory: 20064 KB -BaekjoonHub

---
+++ 백준/Bronze/12780. 원피스/원피스.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine()
+    val h = br.readLine()
+    bw.write("${n.split(h).size - 1}")
+
+    bw.flush()
+    bw.close()
+}
