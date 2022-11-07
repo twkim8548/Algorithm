0077670850a364a1ec4839f74fd6d8bc81240ff4
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-11-07 11:31:35 +0900

    [Bronze II] Title: 이진수 변환, Time: 88 ms, Memory: 12348 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.lang.Math.round
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write(br.readLine().toBigInteger().toString(2))
+
+    bw.flush()
+    bw.close()
+}
