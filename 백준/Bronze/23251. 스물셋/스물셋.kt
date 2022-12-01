7c902b6d93979bc9cdb0357a24dfc2cf0c2dac2f
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-12-01 10:49:22 +0900

    [Bronze II] Title: 스물셋, Time: 472 ms, Memory: 62244 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(br.readLine().toInt()) {
+        bw.write("${23 * br.readLine().toInt()}\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
