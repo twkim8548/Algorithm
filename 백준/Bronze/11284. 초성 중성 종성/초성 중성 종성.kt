5a4ed53272e285b7c59155a5c5de21eacfd21826
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-12-05 11:06:50 +0900

    [Bronze II] Title: 초성 중성 종성, Time: 92 ms, Memory: 12280 KB -BaekjoonHub

---
+++ 백준/Bronze/11284. 초성 중성 종성/초성 중성 종성.kt
@@ -0,0 +1,31 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val char = br.readLine().toCharArray()[0]
+    val initialList = arrayOf("ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
+    val medialList = arrayOf("ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ")
+    val finalList = arrayOf("", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ")
+
+    (char.toInt() - 0xAC00).let {
+        var v = it
+        val f = v % 28
+        v /= 28
+        val m = v % 21
+        v /= 21
+        val i = v % 19
+        bw.write("${initialList[i]}\n${medialList[m]}\n${finalList[f]}")
+    }
+
+
+    bw.flush()
+    bw.close()
+}
