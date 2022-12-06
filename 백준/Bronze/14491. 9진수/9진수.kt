08c714fc6ca812586702bd2c42c0746c53130140
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-12-06 10:30:37 +0900

    [Bronze II] Title: 9진수, Time: 88 ms, Memory: 12352 KB -BaekjoonHub

---
+++ 백준/Bronze/14491. 9진수/9진수.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write(br.readLine().toInt().toString(9))
+
+    bw.flush()
+    bw.close()
+}
