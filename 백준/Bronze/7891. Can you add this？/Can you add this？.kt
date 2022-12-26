fed7784178de87222c2f0913b579fd819da9ee14
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2022-12-26 16:11:40 +0900

    [Bronze IV] Title: Can you add this?, Time: 92 ms, Memory: 12668 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(br.readLine().toInt()) {
+        val token = StringTokenizer(br.readLine())
+        bw.write("${token.nextToken().toBigInteger() + token.nextToken().toBigInteger()}\n")
+    }
+
+    bw.flush()
+    bw.close()
+}
