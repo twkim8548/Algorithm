d2e0c8573a52bd1af668e71008d98a04a828d6e2
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-01-03 10:20:30 +0900

    [Bronze III] Title: 369, Time: 512 ms, Memory: 209200 KB -BaekjoonHub

---
+++ 백준/Bronze/17614. 369/369.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    var answer = 0
+    for (i in 1 .. n) {
+        i.toString().forEach {
+            if (it != '0' && it.toString().toInt() % 3 == 0) {
+                answer ++
+            }
+        }
+    }
+
+    bw.write("$answer")
+
+    bw.flush()
+    bw.close()
+}
