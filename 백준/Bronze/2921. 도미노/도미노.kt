cdc0a9f558cb6725a03e18be62a144ffd4932cd7
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-01-26 22:46:09 +0900

    [Bronze III] Title: 도미노, Time: 100 ms, Memory: 12584 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.kt
@@ -0,0 +1,24 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    var answer = 0
+
+    for (i in 0 .. n) {
+        for (j in i .. n) {
+            answer += j
+            answer += i
+        }
+    }
+    bw.write("$answer")
+
+    bw.flush()
+    br.close()
+}
