ea2cfd9d0ea683ca92900c90a1d174b9db4eec9f
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-02-14 19:24:36 +0900

    Create _14935.kt

---
+++ src/com/taewoong/baekjoon/bronze/_14935.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("FA")
+
+    bw.flush()
+    bw.close()
+}
