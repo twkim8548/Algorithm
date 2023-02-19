c928c541d4e9517bdd06b7a5e0722f969b21d75c
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-02-19 10:09:39 +0900

    Create _26574.kt

---
+++ src/com/taewoong/baekjoon/bronze/_26574.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    repeat(br.readLine().toInt()) {
+        val n = br.readLine().toInt()
+        bw.write("$n $n\n")
+    }
+    bw.flush()
+    bw.close()
+}
