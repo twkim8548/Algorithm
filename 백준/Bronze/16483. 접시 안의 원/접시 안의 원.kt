8115016a21363dd3a4aa486eb3e47abc27890e1d
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-03-03 17:52:36 +0900

    [Bronze III] Title: 접시 안의 원, Time: 88 ms, Memory: 12236 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import kotlin.math.pow
+import kotlin.math.round
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("${round((br.readLine().toInt() / 2.0).pow(2)).toInt()}")
+
+    bw.flush()
+    bw.close()
+}
