3f7260a4775be6b9c1ce825a4424d65984d0a345
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-03-24 21:42:32 +0900

    [Bronze IV] Title: Hello Judge, Time: 88 ms, Memory: 12128 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.kt
@@ -0,0 +1,18 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+import kotlin.math.floor
+import kotlin.math.sqrt
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(br.readLine().toInt()) {
+        bw.write("Hello World, Judge ${it+1}!\n")
+    }
+    bw.flush()
+    br.close()
+}
