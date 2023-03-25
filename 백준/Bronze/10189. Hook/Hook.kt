01c509ea211fa4c7475cd3b5092212886ce4a365
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-03-25 20:55:49 +0900

    [Bronze V] Title: Hook, Time: 80 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+import kotlin.math.floor
+import kotlin.math.sqrt
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write(
+        "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #"
+    )
+    bw.flush()
+    br.close()
+}
