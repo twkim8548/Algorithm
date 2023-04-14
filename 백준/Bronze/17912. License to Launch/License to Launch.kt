8f0659deba4cdc8f62a5e21d57a33aa060d1e281
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-04-14 16:55:08 +0900

    [Bronze III] Title: License to Launch, Time: 332 ms, Memory: 31928 KB -BaekjoonHub

---
+++ 백준/Bronze/17912. License to Launch/License to Launch.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+import kotlin.math.sqrt
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt()
+    val token = StringTokenizer(br.readLine())
+    val array = IntArray(n) { token.nextToken().toInt() }
+
+    bw.write("${array.indexOf(array.min())}")
+
+    bw.flush()
+    bw.close()
+}
