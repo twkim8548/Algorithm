585c298db59b1555faacc0992626bdb2139f7d16
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-04-23 17:56:13 +0900

    [Bronze IV] Title: A Simple Problem., Time: 808 ms, Memory: 152804 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+import kotlin.math.pow
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    repeat(br.readLine().toInt()) {
+        val n = br.readLine().toBigInteger()
+        bw.write("${n * n}\n")
+    }
+
+    bw.flush()
+    br.close()
+}
