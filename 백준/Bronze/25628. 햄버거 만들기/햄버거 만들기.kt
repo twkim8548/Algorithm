0e17ef889e0fcfb67f781661de0daabcf94c3f4d
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-06-03 16:27:22 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 92 ms, Memory: 12272 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.time.OffsetDateTime
+import java.util.StringTokenizer
+import kotlin.math.min
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+
+    bw.write("${min(token.nextToken().toInt() / 2, token.nextToken().toInt())}")
+
+
+    bw.flush()
+    bw.close()
+}
