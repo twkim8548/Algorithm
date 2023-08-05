fe7475939293ed51b60c29cd2074c438040863ac
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-08-05 23:29:50 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 92 ms, Memory: 12256 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    bw.write("${br.readLine().toInt()}\n1")
+
+    bw.flush()
+    br.close()
+}
