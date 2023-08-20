c859f8205cabd0b394c5bdf1840020eaf74ebad5
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-08-20 22:50:33 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 124 ms, Memory: 18476 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val token = StringTokenizer(br.readLine())
+    val array = IntArray(3) { token.nextToken().toInt() }
+
+    bw.write("${array.sorted()[1]}")
+
+    bw.flush()
+    br.close()
+}
