48d22c2b74dd09bd976da631d92a1326e989bc63
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-09-23 07:11:17 +0900

    [Bronze IV] Title: Betting, Time: 92 ms, Memory: 12368 KB -BaekjoonHub

---
+++ 백준/Bronze/24751. Betting/Betting.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.math.BigInteger
+import java.util.StringTokenizer
+import kotlin.math.abs
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = br.readLine().toDouble()
+    bw.write("${100.0 / n}\n${100.0 / (100.0 - n)}")
+
+
+    bw.flush()
+    br.close()
+}
