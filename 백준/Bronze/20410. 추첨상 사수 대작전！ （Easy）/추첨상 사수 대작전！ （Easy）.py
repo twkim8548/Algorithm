d0b505d8c99b321c42ef8ef8e79294950ce55408
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2023-12-12 09:26:41 +0900

    [Bronze III] Title: 추첨상 사수 대작전! (Easy), Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/20410. 추첨상 사수 대작전！ （Easy）/추첨상 사수 대작전！ （Easy）.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+m, seed, x1, x2 = map(int, input().split())
+
+for a in range(m):
+    for c in range(m):
+        if x1 == (a * seed + c) % m and x2 == (a * x1 + c) % m:
+            print(a, c)
+            exit()
