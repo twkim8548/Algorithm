e2b4c49bc454c9027c32b2c45adb450e3b0e31d1
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-04-09 18:17:52 +0900

    _30676.py 업데이트

---
+++ src/com/taewoong/baekjoon/bronze/_30676.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+if 620 <= num <= 780:
+    print("Red")
+elif num >=590:
+    print("Orange")
+elif num >=570:
+    print("Yellow")
+elif num >=495:
+    print("Green")
+elif num >=450:
+    print("Blue")
+elif num >=425:
+    print("Indigo")
+elif num >= 380:
