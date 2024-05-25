f5f7c6ee74d711f346d86572436b86f093390a4f
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-05-25 13:04:46 +0900

    Create _9610.py

---
+++ src/com/taewoong/baekjoon/bronze/_9610.py
@@ -0,0 +1,25 @@
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+axis = 0
+
+for i in range(int(input())):
+    x, y = map(int, input().split())
+    
+    if x == 0 or y == 0:
+        axis += 1
+    elif x > 0 and y > 0:
+        q1 += 1
+    elif x < 0 and y > 0:
+        q2 += 1
+    elif x < 0 and y < 0:
+        q3 += 1
+    elif x > 0 and y < 0:
+        q4 += 1
+
+print(f"Q1: {q1}")
+print(f"Q2: {q2}")
+print(f"Q3: {q3}")
+print(f"Q4: {q4}")
+print(f"AXIS: {axis}")
