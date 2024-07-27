f751f7d265c7220d7efaa3945f26c63529e69f83
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-07-27 14:10:09 +0900

    _31429.py 업데이트

---
+++ src/com/taewoong/baekjoon/bronze/_31429.py
@@ -0,0 +1,24 @@
+n =int(input())
+
+if n == 1:
+    print(12, 1600)
+elif n == 2:
+    print(11, 894)
+elif n == 3:
+    print(11, 1327)
+elif n == 4:
+    print(10, 1311)
+elif n == 5:
+    print(9, 1004)
+elif n == 6:
+    print(9, 1178)
+elif n == 7:
+    print(9, 1357)
+elif n == 8:
+    print(8, 837)
+elif n == 9:
+    print(7, 1055)
+elif n == 10:
+    print(6, 556)
+elif n == 11:
+    print(6, 773)
