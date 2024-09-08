d3e387f85f7e50a85ad933c96ba17238897e26da
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-09-08 22:58:37 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+for _ in range(n):
+    s = input()
+    if s == 'Algorithm':
+        print(204)
+    elif s == 'DataAnalysis':
+        print(207)
+    elif s == 'ArtificialIntelligence':
+        print(302)
+    elif s == 'CyberSecurity':
+        print("B101")
+    elif s == 'Network':
+        print(303)
+    elif s == 'Startup':
+        print(501)
+    else:
+        print(105)
