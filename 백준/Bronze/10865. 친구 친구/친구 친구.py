2194a78ed999251899c1bf7a975d64e9e5fdfc0c
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-09-21 17:17:17 +0900

    [Bronze III] Title: 친구 친구, Time: 792 ms, Memory: 31832 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [0] * n
+for i in range(m):
+    a, b = map(int, input().split())
+    arr[a-1] += 1
+    arr[b-1] += 1
+for i in arr:
+    print(i)
