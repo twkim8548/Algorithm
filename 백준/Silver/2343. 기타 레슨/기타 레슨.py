593ac50765d3e92ea22a6bb3c311d4f2e36356e0
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-10-01 15:50:14 +0900

    [Silver I] Title: 기타 레슨, Time: 428 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.py
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -1,26 +1,28 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int,input().split())
-time = list(map(int,input().split()))
 
-start = max(time)
-end = sum(time)
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
 
-while start <= end:
-    mid = (start + end) // 2 
+low = max(arr)
+high = sum(arr)
+ans = high
 
-    total = 0
+while low <= high:
+    mid = (low + high) // 2
+
     count = 1
-    for t in time:
-        if total + t > mid:
+    tot = 0
+    for i in arr:
+        if tot + i > mid:
             count += 1
-            total = 0
-        total += t 
+            tot = 0
+        tot += i
 
     if count <= m:
         ans = mid
-        end = mid - 1
+        high = mid - 1
     else:
-        start = mid + 1
-    
+        low = mid + 1
+
 print(ans)
