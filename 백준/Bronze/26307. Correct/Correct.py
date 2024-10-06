d232eee254e86c376fc319388d462953bfb0c358
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-10-06 22:26:06 +0900

    [Bronze V] Title: Correct, Time: 68 ms, Memory: 38036 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.py
@@ -0,0 +1,18 @@
+import sys
+from datetime import datetime
+
+input = sys.stdin.readline
+
+def correct(HH, MM):
+    start_time = datetime(year=2022, month=1, day=1, hour=9)
+
+    end_time = datetime(year=2022, month=1, day=1, hour=HH, minute=MM)
+
+    consumed_time = end_time - start_time
+
+    return consumed_time.seconds // 60
+
+
+HH, MM = map(int, input().split())
+
+print(correct(HH=HH, MM=MM))
