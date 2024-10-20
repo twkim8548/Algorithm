1576ed3e411e5bf6032cd56ab42620ed347d31a7
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-10-20 22:26:16 +0900

    [Silver V] Title: 거스름돈, Time: 68 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [-1] * (n + 8)
+
+dp[2] = 1
+dp[4] = 2
+dp[5] = 1
+dp[6] = 3
+dp[7] = 2
+dp[8] = 4
+
+for i in range(9, n+1):
+    dp[i] = min(dp[i-2], dp[i-5]) + 1
+print(dp[n])
