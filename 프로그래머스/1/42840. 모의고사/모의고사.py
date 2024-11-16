559218806ff458b516d8288cd1f6bf89f751c785
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-11-16 12:58:32 +0900

    [level 1] Title: 모의고사, Time: 3.66 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,33 +1,30 @@
 def solution(answers):
-    answer = []
-    person1 = [1,2,3,4,5]
-    person2 = [2,1,2,3,2,4,2,5]
-    person3 = [3,3,1,1,2,2,4,4,5,5]
+    first_answers = [1, 2, 3, 4, 5]
+    second_answers = [2, 1, 2, 3, 2, 4, 2, 5]
+    third_answers = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    tot1, tot2, tot3 = 0, 0, 0
+    first_answer_cnt = 0
+    second_answer_cnt = 0
+    third_answer_cnt = 0
     
     for i in range(len(answers)):
         ans = answers[i]
-        if ans == person1[i%5]:
-            tot1 += 1
-        if ans == person2[i%8]:
-            tot2 += 1
-        if ans == person3[i%10]:
-            tot3 += 1
-    print(tot1, tot2, tot3)
-    if tot1 > tot2 and tot1 > tot3: 
-        answer = [1]
-    elif tot1 == tot2 and tot2 > tot3:
-        answer = [1,2]
-    elif tot1 == tot2 and tot2 == tot3:
-        answer = [1,2,3]
-    elif tot2 > tot1 and tot2 > tot3:
-        answer = [2]
-    elif tot2 == tot3 and tot2 > tot1:
-        answer = [2,3]
-    elif tot3 > tot2 and tot3 > tot1:
-        answer = [3]
-    elif tot1 == tot3 and tot1 > tot2:
-        answer = [1,3]
+        
+        if first_answers[i % len(first_answers)] == ans:
+            first_answer_cnt += 1
+        if second_answers[i % len(second_answers)] == ans:
+            second_answer_cnt += 1
+        if third_answers[i % len(third_answers)] == ans:
+            third_answer_cnt += 1
+    
+    answer = []
+    
+    if max(first_answer_cnt, second_answer_cnt, third_answer_cnt) == first_answer_cnt:
+        answer.append(1)
+    if max(first_answer_cnt, second_answer_cnt, third_answer_cnt) == second_answer_cnt:
+        answer.append(2)
+    if max(first_answer_cnt, second_answer_cnt, third_answer_cnt) == third_answer_cnt:
+        answer.append(3)
+    
     
     return answer
