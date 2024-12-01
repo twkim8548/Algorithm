1cd07a1b2ac2e0ed9cb576ac83259b1805325376
Taewoong Kim <37572328+twkim8548@users.noreply.github.com>
2024-12-01 23:07:33 +0900

    [Bronze III] Title: A/B - 3, Time: 44 ms, Memory: 31120 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.py
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.py
@@ -1,4 +1,10 @@
-a, b = map(int, input().split())
+import sys
 
-print(a//b)
-print(a%b)
+input = sys.stdin.readline
+a, b = map(int, input().split())
+c, d = a//b, a%b
+if a != 0 and d < 0:
+    c, d = c+1, d-b
+
+print(c)
+print(d)
